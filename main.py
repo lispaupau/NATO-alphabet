c71efd1f18ddcdfeeb010da2afdee0ace81d6f24
pndao <pndao@inbox.ru>
2023-10-15 20:37:35 +0300

    update

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input('Enter a word: ').upper()
-
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+while True:
+    word = input('Enter a word: ').upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+    else:
+        print(output_list)
+        break
